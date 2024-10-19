fbf7d66e562782177f465d16d300b24df38e6f67
Your Name <you@example.com>
2024-10-19 13:53:12 +0500

    v3

--- back/upload.go
+++ back/upload.go
@@ -1,10 +1,13 @@
 package back
 
 import (
+	"encoding/csv"
+	"fmt"
 	"io"
 	"net/http"
 	"os"
 	"path/filepath"
+	"time"
 )
 
 func UploadObject(w http.ResponseWriter, r *http.Request) {
@@ -18,7 +21,10 @@ func UploadObject(w http.ResponseWriter, r *http.Request) {
 	objKey := parts[1]
 
 	// Check if the bucket exists
-
+	if _, err := os.Stat("data/" + bucketName); os.IsNotExist(err) {
+		http.Error(w, "Bucket not found\n", http.StatusNotFound)
+		return
+	}
 	// Create the bucket directory if it doesn't exist
 	bucketDir := filepath.Join("data", bucketName)
 	objectPath := filepath.Join(bucketDir, objKey)
@@ -36,4 +42,30 @@ func UploadObject(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Failed to write object data\n", http.StatusInternalServerError)
 		return
 	}
+
+	// Save object metadata to CSV
+	if err := appendObjectMetadata(bucketDir, objKey); err != nil {
+		http.Error(w, "Failed to update object metadata\n", http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Object '%s' uploaded successfully to bucket '%s'\n", objKey, bucketName)
+}
+
+func appendObjectMetadata(bucketDir, objKey string) err {
+	csvPath := filepath.Join(bucketDir, "object.csv")
+
+	file, err := os.OpenFile(csvPath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	// Write the object key and current timestamp
+	if err := writer.Write([]string{objKey, time.Now().Format(time.RFC3339)}); err != nil {
+		return err
+	}
+	return nil
 }
